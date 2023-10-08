849f6098cc41fd409c99b644482a8a8049fcad46
f-sara <jyujyusa.68@gmail.com>
2023-10-08 14:08:02 +0900

    配列sampleDataにサンプルリマインダーを定義 #2

--- Tutorials_ListView/Models/Reminder.swift
+++ Tutorials_ListView/Models/Reminder.swift
@@ -14,3 +14,24 @@ struct Reminder {
     var isComplete: Bool = false
 
 }
+
+#if DEBUG
+
+extension Reminder {
+    static var sampleData = [
+        Reminder(
+            title: "サンプル1",
+            dueDate: Date().addingTimeInterval(800.0),
+            notes: "サンプルサンプル"),
+        Reminder(
+            title: "サンプル2",
+            dueDate: Date().addingTimeInterval(800.0),
+            notes: "サンプルサンプルサンプル"),
+        Reminder(
+            title: "サンプル3",
+            dueDate: Date().addingTimeInterval(800.0),
+            notes: "サンプルサンプルサンプルサンプル")
+    ]
+}
+
+#endif
